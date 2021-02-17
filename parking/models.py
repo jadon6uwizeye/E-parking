c122d6fb8aef58bb21a1e58b94cdba002db37ba9
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-02-18 00:57:47 +0200

    added location model class

--- parking/models.py
+++ parking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.name
