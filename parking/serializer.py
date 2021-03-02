c8e089f941219c65483af9f69001280b5d7b5c3f
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-03-02 18:03:04 +0200

    added login api

--- parking/serializer.py
+++ parking/serializer.py
@@ -21,9 +21,9 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 # User serializer
 class UserSerializer(serializers.ModelSerializer):
-     class Meta:
+    class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        fields = '__all__'
 
 # login serializer
 class LoginSerializer(serializers.ModelSerializer):
@@ -34,7 +34,7 @@ class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
-
+        
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
--- smart/settings.py
+++ smart/settings.py
@@ -133,11 +133,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user.User'
 REST_FRAMEWORK = {
+     'DEFAULT_PERMISSION_CLASSES': [
+         'rest_framework.permissions.IsAuthenticated',
+         'rest_framework.permissions.IsAdminUser',
+         ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'authentication.backends.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
